a81085115510f1ae229475c931663c178348bf5c
chandan <chandan.kr8413@gmail.com>
2021-07-28 17:58:57 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,87 @@
-import logo from './logo.svg';
+
+//import logo from './logo.svg';
 import './App.css';
+import Todos from "./Todos";
+import AddTodo from "./AddTodo";
+//import TodoItem from "./TodoItem";
+import image from './image/pic1.jpg';
+import React, { useState, useEffect } from 'react';
+
+import About from "./About";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+
 
 function App() {
+  const onDelete = (todo) => {
+    console.log("I am ondelete fo todo", todo);
+    //deleting this way in react does not work
+    // let index = todos.indexOf(todo);
+    // todos.splice(index, 1);
+
+    setTodos(todos.filter((e) => {
+      return e !== todo; //call set todo
+    }))
+    console.log("deleted", todos)
+    localStorage.setItem("todos", JSON.stringify(todos));
+
+  }
+
+  const addTodo = (title, desc) => {
+    console.log("I am adding this list", title, desc)
+    let sno;
+    if (todos.length === 0) {
+      sno = 0;
+    }
+    else {
+      sno = todos[todos.length - 1].sno + 1;
+    }
+
+    const myTodo = {
+      sno: sno,
+      title: title,
+      desc: desc,
+    }
+    setTodos([...todos, myTodo]);
+    console.log(myTodo);
+  }
+  
+  const [todos, setTodos] = useState([
+    
+  ])
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos])
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Switch>
+          <Route exact path="/" render={() => {
+            return (
+              <>
+              <div className="image">
+              <img src={image} className="image"></img>
+              </div>
+                <AddTodo addTodo={addTodo} />
+                <Todos todos={todos} onDelete={onDelete} />
+              </>)
+          }}>
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
+        </Switch>
+      </Router>
+
+    </>
   );
 }
 
